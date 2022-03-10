92f46d28bbcd6fc032edbfbf7818bd2699b5f10f
samir_yakhlef <samir.yakhlef30@gmail.com>
2022-03-10 19:04:31 +0100

    phpunit

---
+++ test/FizzbuzzTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+
+class FizzbuzzTest extends TestCase
+{
+    public function testFizzbuzz()
+    {
+        self::assertTrue(false);
+    }
+};
