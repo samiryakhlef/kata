6ec40830b706acc48404e3beff4f514cc68db882
samir_yakhlef <samir.yakhlef30@gmail.com>
2022-03-16 19:32:12 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- test/FizzbuzzTest.php
+++ test/FizzbuzzTest.php
@@ -14,21 +14,24 @@ class FizzbuzzTest extends TestCase
     public function testFizzbuzz()
     {
 
-        $this->assertEquals([1, 2, 'fizz', 4, 'buzz'], fizzbuzz());
+        $this->assertEquals([1, 2, 'fizz', 4, 'buzz', 'fizz'], fizzbuzz());
     }
 };
 
 function fizzbuzz()
 {
     $out = [];
-    for ($i = 1; $i <= 5; $i++) {
+    for ($i = 1; $i <= 6; $i++) {
 
         if ($i === 3) {
             $out[] = 'fizz';
 
         } else if ($i === 5) {
             $out[] = 'buzz';
-            
+
+        } else if ($i === 6) {
+            $out[] = 'fizz';
+
         } else {
             $out[] = $i;
         }
